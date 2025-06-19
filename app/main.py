bdd9424405603f5cdab3c508d29ca21eba819211
Hamster <dtaranenko137@gmail.com>
2025-06-19 11:28:45 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cash_storeg = {}
+
+    def wrapper(*args, **kwargs) -> Callable:
+        key = (args, tuple((kwargs.items())))
+        if key in cash_storeg:
+            print("Getting from cache")
+            return cash_storeg[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cash_storeg[key] = result
+        return result
+    return wrapper
